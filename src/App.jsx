2180c7d938cb1ad656a03231b95294e519f5b8ef
InshutiSouvede <isouvede2@gmail.com>
2024-07-24 15:06:46 +0200

    checking answers from the source

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ function App() {
   const [markedData,setMarkedData] = useState([])
   const [data,setData] = useState(()=>initialData())
 
-  const question ="How would one say goodbye in Spanish?"
-  const answers = ['Adiós','Hola','Au Revoir','Salir'] 
-
   console.log("This is what you start with",data)
 
   function initialData(){
@@ -28,8 +25,8 @@ function App() {
   function checkAnswers(){
     // The order of response array is the same order questions were given
     let nailedIt = false
-    const correctedResponse = responses.map((response)=>{
-      if(response.givenAnswer === correctAnswer){
+    const correctedResponse = responses.map((response,i)=>{
+      if(response.givenAnswer === data[i].correctAnswer){
         nailedIt = true
         return {...response,correct:true}
       }
@@ -51,11 +48,11 @@ function App() {
         return !found?[...prevResponses,newResponse]:updatedResponse
     })
   }
+  const qandAs = data.map((data)=><QandA question= {data.question} answers= {data.allAnswers} updateResponses={updateResponses} />)
   return (
-    <div className='flex flex-col items-center w-3/5 p-10 mt-20 m-auto border '>
-     <QandA question= {data[0].question} answers= {data[0].allAnswers} updateResponses={updateResponses} />         
-     <QandA question= {data[1].question} answers= {data[1].allAnswers} updateResponses={updateResponses} />         
-     <button onClick={checkAnswers} className="bg-[#4D5B9E] rounded-lg px-5 py-2 m-auto my-5 text-white font-semibold">Check answers</button>
+    <div className='flex flex-col w-3/5 p-10 mt-20 m-auto border '>
+      {qandAs}
+    <button onClick={checkAnswers} className="bg-[#4D5B9E] rounded-lg px-5 py-2 m-auto my-5 text-white font-semibold">Check answers</button>
     </div>
   )
 }
--- src/compontents/Answer.jsx
+++ src/compontents/Answer.jsx
@@ -1,7 +1,7 @@
 export default function Answer(props){
     return (
         <div className={`w-max rounded-lg ${props.selected?"bg-[#D6DBF5]":"border border-[#4D5B9E]"}`}>
-            <button onClick={props.handleClick} className="min-w-24 text-[#293264] ">{props.value}</button>
+            <button onClick={props.handleClick} className="text-[#293264] px-5 ">{props.value}</button>
         </div>
     )
 }
