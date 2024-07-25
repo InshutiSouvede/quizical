a40a4bfe4ac6a6ad6954df057634a87fcc256a00
InshutiSouvede <isouvede2@gmail.com>
2024-07-25 17:53:03 +0200

    highligt correct and wrong answers when check answers is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import QandA from './compontents/QandA'
-import Button from './compontents/Button'
 import quizData from './data'
 function App() {
-  const [responses,setResponses] = useState([])
-  const [markedData,setMarkedData] = useState([])
+  const [done,setDone] = useState(false)
   const [data,setData] = useState(()=>initialData())
 
   console.log("This is what you start with",data)
@@ -23,32 +21,9 @@ function App() {
   }
 
   function checkAnswers(){
-    // The order of response array is the same order questions were given
-    let nailedIt = false
-    const correctedResponse = responses.map((response,i)=>{
-      if(response.givenAnswer === data[i].correctAnswer){
-        nailedIt = true
-        return {...response,correct:true}
-      }
-      return response  
-    })
-    console.log("These are the responses you provided:",correctedResponse)
+    setDone(true)
   }
-
-  function updateResponses(newResponse){
-    setResponses(prevResponses=>{
-      let found = false
-      const updatedResponse = prevResponses.map((response)=>{
-        if(response.id === newResponse.id){
-          found = true
-          return newResponse
-        }
-        return response        
-      })
-        return !found?[...prevResponses,newResponse]:updatedResponse
-    })
-  }
-  const qandAs = data.map((data)=><QandA question= {data.question} answers= {data.allAnswers} updateResponses={updateResponses} />)
+  const qandAs = data.map((data)=><QandA done={done} questionData= {data} />)
   return (
     <div className='flex flex-col w-3/5 p-10 mt-20 m-auto border '>
       {qandAs}
--- src/compontents/QandA.jsx
+++ src/compontents/QandA.jsx
@@ -3,11 +3,16 @@ import Answer from "./Answer";
 import { useEffect, useState } from "react";
 
 export default function QandA(props) {
-    const [selectedAnswer, setSelectedAnswer] = useState({id:nanoid(),question:props.question, givenAnswer:'',correct:false})
+    const [selectedAnswer, setSelectedAnswer] = useState({id:nanoid(),question:props.questionData.question, givenAnswer:'',correct:false})
     const [answers, setAnswers] = useState(() => allOptions())
     //see if you can handle this in App.jsx
     function allOptions() {
-        return props.answers.map((el) => ({ value: el, isSelected: false, id: nanoid() }))
+        return props.questionData.allAnswers.map((el) => {
+            if(el==props.questionData.correctAnswer){
+                return { value: el, isSelected: false, isCorrect:true, id: nanoid() }
+            }            
+            return { value: el, isSelected: false, isCorrect:false, id: nanoid() }
+        })
     }
     function select(event) {
         const ans = event.target.textContent
@@ -25,14 +30,14 @@ export default function QandA(props) {
                 return ans
             })
         })
-        props.updateResponses(selectedAnswer)
+        // props.updateResponses(selectedAnswer)
     }, [selectedAnswer])
     return (
         <div className="flex flex-col gap-5 border-b-2 border-b-[#DBDEF0]  py-5">
-            <h1 className="font-semibold text-2xl text-[#293264]">{props.question}</h1>
+            <h1 className="font-semibold text-2xl text-[#293264]">{props.questionData.question}</h1>
             <div className="flex gap-5 flex-wrap">
                 {
-                    answers.map((el) => <Answer key={el.id} selected={el.isSelected} value={el.value} handleClick={(event) => select(event)} />)
+                    answers.map((el) => <Answer key={el.id} done={props.done} isCorrect={el.isCorrect} selected={el.isSelected} value={el.value} handleClick={(event) => select(event)} />)
                 }
             </div>
 
