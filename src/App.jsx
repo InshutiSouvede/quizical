9886c734350a472f14c5377f91cf39af7f86f4e4
InshutiSouvede <isouvede2@gmail.com>
2024-07-25 18:50:41 +0200

    Add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import quizData from './data'
 function App() {
   const [done,setDone] = useState(false)
   const [data,setData] = useState([])
+  const [started,setStarted] = useState(false)
 
   function initialData(data){
     return data.results.map((res)=>{      
@@ -20,6 +21,10 @@ function App() {
 
   function checkAnswers(){
     setDone(true)
+  }
+  function startQuiz(){
+    setDone(false)
+    setStarted(prev=>!prev)
   }
   const qandAs = data.map((data)=><QandA done={done} questionData= {data} />)
   useEffect(()=>{
@@ -39,10 +44,22 @@ function App() {
     getData()
   },[])
   return (
+    <>
+    {!started?(<div className='w-3/5 text-[#293264] gap-5 p-10 m-auto flex flex-col mt-60 items-center'>
+      <h1 className=' font-semibold text-3xl'>Quizzical</h1>
+      <p>You will be given 5 multiple questions related to computer science</p>
+      <button onClick={startQuiz} className="bg-[#4D5B9E] rounded-lg px-5 py-2 m-auto my-5 text-white font-semibold">Start quiz</button>
+    </div>):
     <div className='flex flex-col w-3/5 p-10 mt-20 m-auto border '>
       {qandAs}
-    <button onClick={checkAnswers} className="bg-[#4D5B9E] rounded-lg px-5 py-2 m-auto my-5 text-white font-semibold">Check answers</button>
-    </div>
+      <div className="flex justify-between">
+      <button onClick={checkAnswers} className="bg-[#4D5B9E] rounded-lg px-5 py-2 m-auto my-5 text-white font-semibold">Check answers</button>
+      <button onClick={startQuiz} className="bg-[#a4a5a9] rounded-lg px-5 py-2 m-auto my-5 text-[#4D5B9E] font-semibold">Exit</button>
+    
+      </div>
+    </div>}
+    </>
+    
   )
 }
 
